59944c47512dbc60052ba264efbd2b3444bde122
Shadab Ahmed <shadab.ansari@gmail.com>
2018-12-12 09:16:22 -0800

    Add ability to replace beautified comments when line changes

--- src/extension.ts
+++ src/extension.ts
@@ -2,16 +2,24 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  function getComment(text: String): String {
+  function getComment(text: string): string {
     let comment = [];
     for(let i = 0; i < text.length; i++){
       comment.push('#');
     }
     return comment.join('');
+  }
+
+  function isBeautifiedComment(text: string): boolean {
+    for(let i = 0; i < text.length; i++) {
+      if(text[i] != '#') {
+        return false;
+      }
+    }
+    return true;
   }
 
   let command = vscode.commands.registerCommand('extension.beautifyTerraformComments', () => {
@@ -22,14 +30,23 @@ export function activate(context: vscode.ExtensionContext) {
       const edit = new vscode.WorkspaceEdit();
       for(let i = 0; i < document.lineCount; i++) {
         let line = document.lineAt(i);
-        if(line.text.startsWith('#'))
-        {
-          let prevNotComment = i == 0 || (!document.lineAt(i - 1).text.startsWith('#'));
-          let nextNotComment = (i == document.lineCount - 1) || (!document.lineAt(i + 1).text.startsWith('#'));
-          if(prevNotComment && nextNotComment){
-            let comment = getComment(line.text);
+        if(line.text.startsWith('#') && !isBeautifiedComment(line.text)) {
+          let prevLine = i > 0 ? document.lineAt(i - 1) : null;
+          let nextLine = i < document.lineCount - 1 ? document.lineAt(i + 1) : null;
+          let prevLineIsComment = prevLine && prevLine.text.startsWith('#');
+          let nextLineIsComment = nextLine && nextLine.text.startsWith('#');
+          let comment = getComment(line.text);
+          if(prevLine && prevLineIsComment && isBeautifiedComment(prevLine.text)){
+            edit.replace(document.uri, prevLine.range, comment);
+          }
+          else if(!prevLineIsComment) {
             edit.insert(document.uri, line.range.start, comment +'\n');
-            edit.insert(document.uri, line.range.end, '\n' + comment);              
+          }
+          if(nextLine && nextLineIsComment && isBeautifiedComment(nextLine.text)){
+            edit.replace(document.uri, nextLine.range, comment);
+          }
+          else if(!nextLineIsComment) {
+            edit.insert(document.uri, line.range.end, '\n' + comment);
           }
         }
       }
